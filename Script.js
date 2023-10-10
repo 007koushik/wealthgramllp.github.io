52f0527783624e65068b72223edf77e3ca3dd2c1
007koushik <144988922+007koushik@users.noreply.github.com>
2023-10-10 11:03:50 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -24,6 +24,30 @@ window.addEventListener('scroll', () => {
 
 
 
+// Image Slider Script 
+
+
+const container = document.querySelector('.carousel-container');
+        let currentIndex = 0;
+        const slides = document.querySelectorAll('.carousel-slide');
+
+        function changeSlide(direction) {
+            currentIndex += direction;
+            if (currentIndex < 0) {
+                currentIndex = slides.length - 1;
+            }
+            if (currentIndex >= slides.length) {
+                currentIndex = 0;
+            }
+            const translateX = -currentIndex * 100;
+            container.style.transform = `translateX(${translateX}%)`;
+        }
+
+        setInterval(() => {
+            changeSlide(1);
+        }, 1000);
+
+
 
 
 
